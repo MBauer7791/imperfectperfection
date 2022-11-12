8ec348741e77cc5fe548063236d31d6d5c8f3db0
Marc Bauer <marc.jahservant@gmail.com>
2022-11-11 19:27:18 -0500

    first major push. working on gallery

---
+++ js/imper.js
@@ -0,0 +1,27 @@
+// show controls
+this.carouselOptions = ['previous', 'add', 'play', 'next'];
+// replace images here
+this.carouselData = [
+  {
+    'id': '1',
+    'src': '1.jpg',
+  },
+  {
+    'id': '2',
+    'src': '2.jpg',
+  },
+  {
+    'id': '3',
+    'src': '3.jpg',
+  },
+  {
+    'id': '4',
+    'src': '4.jpg',
+  },
+  {
+    'id': '5',
+    'src': '5.jpg',
+  }
+];
+// items in view
+this.carouselInView = [1, 2, 3, 4, 5];
