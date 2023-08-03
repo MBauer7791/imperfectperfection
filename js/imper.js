87837f8aa29c4b3559280d6a0929c1aeed98f0db
Marc Bauer <official.marc.bauer@gmail.com>
2023-08-03 14:14:48 -0400

    -m "Pics sorted, +coco, leprd,Pivory,Swood,BraWal"

--- js/imper.js
+++ js/imper.js
@@ -1,6 +1,41 @@
 // When the user clicks on <div>, open the popup
 
-function showImg() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
+function showImg1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+
+function showImg2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+
+function showImg3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+
+function showImg4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
+
+function showImg5() {
+  var popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+}
+
+function showImg6() {
+  var popup = document.getElementById("myPopup6");
+  popup.classList.toggle("show");
+}
+
+function showImg7() {
+  var popup = document.getElementById("myPopup7");
+  popup.classList.toggle("show");
+}
+
+function showImg8() {
+  var popup = document.getElementById("myPopup8");
+  popup.classList.toggle("show");
+}
