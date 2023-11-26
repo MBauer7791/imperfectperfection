b9872e59d937de03e83a2da49e970b432e350765
Marc <marcbauer6@hotmail.com>
2023-11-25 21:26:39 -0500

    woods built - modal

--- js/imper.js
+++ js/imper.js
@@ -70,6 +70,28 @@ function showCuff1() {
   popup.classList.toggle("show");
 }
 
+// Get the modal
+var modal = document.getElementById('ambrosia-maple');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('ambrosia-maple');
+var modalImg = document.getElementById("ambrosia-maple");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+
 //$('#exampleModal').on('show.bs.modal', function (event) {
   //var button = $(event.relatedTarget) // Button that triggered the modal
   //var recipient = button.data('whatever') // Extract info from data-* attributes
@@ -81,8 +103,8 @@ function showCuff1() {
 //})
 
 // jquery setup
-$('#snakewood').modal('show')
+//$('#snakewood').modal('show')
 
-$(document).ready(function(){
-  $("#snakewood").modal("show");
-});
+//$(document).ready(function(){
+//  $("#snakewood").modal("show");
+//});
